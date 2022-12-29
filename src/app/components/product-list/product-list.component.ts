c08f17504fe24ea71af46e4edec370e746fb5f97
Surya Madhavan <surya.madhavan@capgemini.com>
2022-12-29 10:31:20 +0530

    second half - 28/12/22

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number;
+  currentCategoryName: string;
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService, private route: ActivatedRoute){}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    })
     this.listProducts();
   }
 
   listProducts(){
-    this.productService.getProductList().subscribe(
+    // check if id is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if(hasCategoryId){
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
+    } 
+    else {
+      this.currentCategoryId = 1;
+      this.currentCategoryName = "Books";
+    }
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
